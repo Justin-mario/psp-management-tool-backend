178806d52023ebfceb6f79992fd810d63e13b56e
Chigozie Nwachukwu <cn23aah@herts.ac.uk>
2024-07-22 21:38:56 +0100

    psp management tool first commit

---
+++ src/main/java/com/pspmanagement/PspManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.pspmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PspManagementToolApplication.class, args);
+    }
+
+}
