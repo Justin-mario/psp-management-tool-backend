ce2cd0a4bf1f80bb6bc79fde1fe153d5428bce8e
Chigozie Nwachukwu <cn23aah@herts.ac.uk>
2024-08-11 15:10:34 +0100

    added project time log entity

--- src/main/java/com/pspmanagement/dto/requestdto/ChangePasswordRequest.java
+++ src/main/java/com/pspmanagement/dto/requestdto/ChangePasswordRequest.java
@@ -1,11 +1,16 @@
 package com.pspmanagement.dto.requestdto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChangePasswordRequest {
 
     private String newPassword;
+
 }
---
+++ src/main/java/com/pspmanagement/model/constant/ProjectPhase.java
@@ -0,0 +1,5 @@
+package com.pspmanagement.model.constant;
+
+public enum ProjectPhase {
+    PLANNING, DESIGN, CODE, COMPILE, TEST, POSTMORTEM
+}
